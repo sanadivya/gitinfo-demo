37c048e12ce303ce143726e1d8d43cd922742a4d
Divya-Sana <divya.sana@cubixcorp.com>
2025-07-01 11:59:57 +0530

    changes

--- fetch_github_meta.sh
+++ fetch_github_meta.sh
@@ -29,22 +29,22 @@ echo "$COMMIT_DATA" | jq -r '.commit.message'
 echo "🔹 Commit Author:"
 echo "$COMMIT_DATA" | jq -r '.commit.author.name'
 
-# echo ""
-# echo "Checking PRs associated with the commit..."
-# PR_API="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/commits/${COMMIT_SHA}/pulls"
-# PR_DATA=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.groot-preview+json" "$PR_API")
+echo ""
+echo "Checking PRs associated with the commit..."
+PR_API="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/commits/${COMMIT_SHA}/pulls"
+PR_DATA=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.groot-preview+json" "$PR_API")
 
-# PR_COUNT=$(echo "$PR_DATA" | jq 'length')
+PR_COUNT=$(echo "$PR_DATA" | jq 'length')
 
-# if [ "$PR_COUNT" -gt 0 ]; then
-#   echo "✅ PR Info Found:"
-#   echo "PR Number:   $(echo "$PR_DATA" | jq -r '.[0].number')"
-#   echo "PR Title:    $(echo "$PR_DATA" | jq -r '.[0].title')"
+if [ "$PR_COUNT" -gt 0 ]; then
+  echo "✅ PR Info Found:"
+  echo "PR Number:   $(echo "$PR_DATA" | jq -r '.[0].number')"
+  echo "PR Title:    $(echo "$PR_DATA" | jq -r '.[0].title')"
 #   echo "PR Author:   $(echo "$PR_DATA" | jq -r '.[0].user.login')"
 #   echo "Merged By:   $(echo "$PR_DATA" | jq -r '.[0].merged_by.login')"
-#   echo "Source → Target: $(echo "$PR_DATA" | jq -r '.[0].head.ref') → $(echo "$PR_DATA" | jq -r '.[0].base.ref')"
-# else
-#   echo "⚠️  No PR associated with this commit."
+  echo "Source → Target: $(echo "$PR_DATA" | jq -r '.[0].head.ref') → $(echo "$PR_DATA" | jq -r '.[0].base.ref')"
+else
+  echo "⚠️  No PR associated with this commit."
 
-# fi
+fi
 
